b164b7c091d622bee94137a07a0f41ba6e4f8efd
Deivison Oliveira <deivisonoliveira.info@gmail.com>
2022-01-13 14:45:31 -0400

    configurando o spring e executando hello world

---
+++ WebContent/WEB-INF/views/ok.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>Hello World Spring 4!</h1>
+
+</body>
+</html>
---
+++ src/br/com/contas/controller/OlaMundoController.java
@@ -0,0 +1,14 @@
+package br.com.contas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundoController {
+	
+	@RequestMapping("/olaMundoSpring")
+	public String olaMundo() {
+		System.out.println("Executando primeira lógica Spring");
+		return "ok";
+	}
+}
